a0bddedd57e93077b40333eb9cd281ea14040b1a
Danish Imam <96104496+danishimam@users.noreply.github.com>
2025-02-26 10:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let inputName = document.getElementById("fname");
 
-inputName.addEventListener("keyup", function() {
+inputName.addEventListener("blur", function() {
 	inputName.value = inputName.value.toUpperCase();
 });
