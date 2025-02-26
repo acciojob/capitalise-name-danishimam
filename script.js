dfb0193a2680b993453edac897e8469c3116240b
Danish Imam <96104496+danishimam@users.noreply.github.com>
2025-02-26 10:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let inputName = document.getElementById("fname")
+
+inputName.addEventListener("keyup", (event) => {
+	event.preventDefault()
+	inputName.value = inputName.value.toUpperCase()
+});
