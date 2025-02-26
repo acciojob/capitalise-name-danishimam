fb331b0e8b965d45eed312dd89bbe580b6b587db
Danish Imam <96104496+danishimam@users.noreply.github.com>
2025-02-26 10:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let inputName = document.getElementById("fname")
+let inputName = document.getElementById("fname");
 
 inputName.addEventListener("keyup", function() {
-	inputName.value = inputName.value.toUpperCase()
-})
+	inputName.value = inputName.value.toUpperCase();
+});
